ba0d77f40c7408ab3ccb4a7385b69aa2fb19c6d6
Oleg Koval <gevin.gl@gmail.com>
2022-04-29 00:33:26 +0300

    bukld

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import "./App.scss";
 import CalcMainPage from "./pages/calc-main/calc-main.component";
 
+import styled from "styled-components";
+
+const Text = styled.div`
+color
+`;
+
 function App() {
   return (
     <div className="App">
